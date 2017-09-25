cabf8432f149d86db4bb1cabc1cec411830826aa
Andreas Frass <github@pifrasso.de>
2017-09-25 09:28:06 +0200

    First commit

---
+++ src/main/java/de/pifrasso/Sample.java
@@ -0,0 +1,21 @@
+package de.pifrasso;
+
+import org.springframework.http.MediaType;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Sample
+{
+    @RequestMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
+    public String home() {
+        return "Nothing here. Go to <a href='/sample'>/sample</a>";
+    }
+    static public void main(String[] args) throws Exception
+    {
+        SpringApplication.run(Sample.class, args);
+    }
+}
